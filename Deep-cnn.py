5a17e6ac45329cbdab73a58de1850f4bb61e885c
ErickVonDircksen <83781289+ErickVonDircksen@users.noreply.github.com>
2022-12-04 18:15:06 -0300

    Update Deep-cnn.py

--- Deep-cnn.py
+++ Deep-cnn.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 import os
-import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import keras
+from keras.layers import  Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+#--------------------------------------------------------------------- #
+# Erick H. Dircksen, Christian A. Carneiro, Raian A. Moretti 
+#--------------------------------------------------------------------- #
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 #-------- tensorflow v2.8.0-----cuda v11.0.194-------cDNN v8.0-------#
 #gpus = tf.config.experimental.list_physical_devices('GPU')
 #for gpu in gpus:
@@ -17,12 +22,8 @@ data = tf.keras.utils.image_dataset_from_directory('data',batch_size=32,shuffle=
 
 #pré porcessamento
 data = data.map(lambda x,y:(x/255,y)) # normaliza os valores da imagem, para melhorar o desempenho.
-dataIterator = data.as_numpy_iterator() # cria im iterator pra podermos visualizar os batches de dados
-batch = dataIterator.next()             # mostra um batch 
-#print(batch[1])                        # batch[1]são os labels, batch[0]são as imagens, cada nº de label representa uma classe(tipo de peixe no nosso caso).
-
-#  divide as imagens em categorias, TREINAMENTO E TESTAGEM, devem estar embaralhas neste ponto
-#print(len(data)) # numero de batches 
+#dataIterator = data.as_numpy_iterator() # cria im iterator pra podermos visualizar os batches de dados
+           
 
 trainSize = int(len(data)*.7)   # 70% para treinamento
 validSize = int(len(data)*.2)+1 # 20% para validação
@@ -38,39 +39,40 @@ test =  data.skip(trainSize+validSize).take(testSize)# o mesmo para os batches d
 # AI TIME!!!
 
 model = tf.keras.models.Sequential() # inicia o modelo
-#adiciona camadas ao modelo;
-model.add(tf.keras.layers.Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
-model.add(tf.keras.layers.Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
-model.add(tf.keras.layers.MaxPooling2D())
-model.add(tf.keras.layers.Conv2D(32, (3,3), 1, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D())
-model.add(tf.keras.layers.Conv2D(16, (3,3), 1, activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D())
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(256, activation='relu'))
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(tf.keras.layers.Dense(256,activation='relu',input_shape=(256,256,3)))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(3, activation='softmax'))
 
 
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy']) #compila o modelo
+model.compile(loss=keras.losses.sparse_categorical_crossentropy,optimizer='adam', metrics=['accuracy']) #compila o modelo
 model.summary() #mostra os resultos do modelo.
 
 logdir='logs'   #cria logs pra fine tunning do moledo
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
-hist = model.fit(train, epochs=20, validation_data=val,verbose="auto") # treinamento 
+hist = model.fit(train, epochs=15, validation_data=val,verbose="auto") # treinamento 
+metric=  tf.keras.metrics.SparseCategoricalCrossentropy()
 
 # plota o resultado
-fig = plt.figure()
-plt.plot(hist.history['loss'], color='teal', label='loss')
-plt.plot(hist.history['accuracy'], color='green', label='accu')
-plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
-fig.suptitle('Loss', fontsize=20)
+plt.plot(hist.history['loss'], label='Loss training data')
+plt.plot(hist.history['val_loss'], label='Loss Validation Data')
+plt.plot(hist.history['accuracy'], label='Accuracy training data')
+plt.title('Model performance for 3D MNIST Keras Conv3D example')
+plt.ylabel('Loss value')
+plt.xlabel('No. epoch')
 plt.legend(loc="upper left")
 plt.show()
 
 
 
-# evaluete
+# evaluate
 pre = tf.keras.metrics.Precision()
 re = tf.keras.metrics.Recall()
 acc = tf.keras.metrics.BinaryAccuracy()
@@ -86,3 +88,4 @@ print(pre.result(), re.result(), acc.result())
 
 #save the model
 model.save(os.path.join('models','imageclassifier.h5'))
+
