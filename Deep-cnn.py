7a5612eff63fb4750a1a67a10d2853be442fc668
ErickVonDircksen <83781289+ErickVonDircksen@users.noreply.github.com>
2022-12-13 13:46:11 -0300

    Update Deep-cnn.py

--- Deep-cnn.py
+++ Deep-cnn.py
@@ -10,7 +10,7 @@ import cv2
 # Erick H. Dircksen, Christian A. Carneiro, Raian A. Moretti 
 #--------------------------------------------------------------------- #
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-#-------- tensorflow v2.8.0-----cuda v11.0.194-------cDNN v8.0-------#
+#-------- tensorflow v2.11.0-----cuda v11.0.194-------cDNN v8.0-------#
 #gpus = tf.config.experimental.list_physical_devices('GPU')
 #for gpu in gpus:
  #   tf.config.experimental.set_memory_growth(gpu,True)
@@ -19,7 +19,7 @@ data_dir = 'data'
 #tratamento dos imgs
 
 #loading com keras
-data = tf.keras.utils.image_dataset_from_directory('data',batch_size=32,shuffle=True) # Keras cria um dataset com labels, resized images,batch size etc... 
+data = tf.keras.utils.image_dataset_from_directory('data',batch_size=64,shuffle=True) # Keras cria um dataset com labels, resized images,batch size etc... 
 
 class_names = data.class_names
 print(class_names)
@@ -38,33 +38,37 @@ val = data.skip(trainSize).take(validSize)         # pega os batches para valida
 
 model = tf.keras.models.Sequential() # inicia o modelo
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(256,256,3)))
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
-
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(64, kernel_size=(3, 3), strides=(2,2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(128, kernel_size=(3, 3), strides=(2,2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
-model.add(Conv2D(256, kernel_size=(3, 3), strides=(2,2), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dropout(0.2))
+model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#model.add(Dropout(0.15))
 model.add(Flatten())
-model.add(tf.keras.layers.Dense(128,activation='relu'))
-# model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(512,activation='relu'))
+model.add(tf.keras.layers.Dense(256, activation='relu'))
+
 model.add(tf.keras.layers.Dense(6, activation='softmax'))
 
 model.compile(loss=keras.losses.sparse_categorical_crossentropy,optimizer='adam', metrics=['accuracy'],) #compila o modelo
 model.summary() #mostra os resultos do modelo.
 
-hist = model.fit(train, epochs=15, validation_data=val,verbose="auto",) # treinamento 
-model.save(os.path.join('models','imageclassifier-3-argumented.h5'))
+es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=5)
+mc = tf.keras.callbacks.ModelCheckpoint(os.path.join('models','best_model-TEST.h5'), monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
+
+hist = model.fit(train, epochs=20, validation_data=val,verbose="auto",callbacks=[mc]) # treinamento 
+model.save(os.path.join('models','imageclassifier-dataset-bom-45k.h5'))
 # plota o resultado
 plt.plot(hist.history['loss'], label='Loss training data')
 plt.plot(hist.history['val_loss'], label='Loss Validation Data')
@@ -76,10 +80,7 @@ plt.legend(loc="upper left")
 plt.show()
 
 
-
-
 # evaluate
 loss,accuracy = model.evaluate(val) # testa dois batches
 print(loss,accuracy) #mostra os resultados do teste
 
-#save the model
