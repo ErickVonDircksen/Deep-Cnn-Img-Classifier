52c2d4586636d8420ba734acc1369ea23b90fc8f
ErickVonDircksen <83781289+ErickVonDircksen@users.noreply.github.com>
2023-01-12 22:44:27 -0300

    Update Deep-cnn.py

--- Deep-cnn.py
+++ Deep-cnn.py
@@ -1,42 +1,35 @@
 import tensorflow as tf
 import os
-import numpy as np
 from matplotlib import pyplot as plt
 import keras
-from keras.layers import  Dropout, Flatten
+from keras.layers import Flatten
 from keras.layers import Conv2D, MaxPooling2D
-import cv2
 #--------------------------------------------------------------------- #
-# Erick H. Dircksen, Christian A. Carneiro, Raian A. Moretti 
-#--------------------------------------------------------------------- #
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-#-------- tensorflow v2.11.0-----cuda v11.0.194-------cDNN v8.0-------#
-#gpus = tf.config.experimental.list_physical_devices('GPU')
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1" uncoment this to run on your CPU
+#-------- tensorflow-gpu-2.10.0-----cuda v11.0.194-------cDNN v8.0-------Python 3.10.6#
+#gpus = tf.config.experimental.list_physical_devices('GPU') uncoment this section to prevent  use too much memory from your gpu
 #for gpu in gpus:
  #   tf.config.experimental.set_memory_growth(gpu,True)
 #--------------------------------------------------------------------- #
-data_dir = 'data'
-#tratamento dos imgs
+data_dir = 'data' # the directory that haves N directories with the images
 
-#loading com keras
-data = tf.keras.utils.image_dataset_from_directory('data',batch_size=64,shuffle=True) # Keras cria um dataset com labels, resized images,batch size etc... 
+#loading with keras
+data = tf.keras.utils.image_dataset_from_directory('data',batch_size=64,shuffle=True) # Keras creates a dataset with labels, resized images,batch size etc... 
 
-class_names = data.class_names
+class_names = data.class_names # get the names of eatch class  
 print(class_names)
 
-#pré porcessamento
-data = data.map(lambda x,y:(x/255,y)) # normaliza os valores da imagem, para melhorar o desempenho.
-# dataIterator = data.as_numpy_iterator() # cria um iterator pra podermos visualizar os batches de dados   
+#pre-processing
+data = data.map(lambda x,y:(x/255,y)) # normalize the image values ​​to improve performance.
 
-trainSize = int(len(data)*.80) # 80% para treinamento
-validSize = int(len(data)*.20) # 20% para validação
+trainSize = int(len(data)*.80) # 80% for training
+validSize = int(len(data)*.20) # 20% for validation
 
-train = data.take(trainSize)                         # pega os batches para treinaemento
-val = data.skip(trainSize).take(validSize)         # pega os batches para validação pulandos os q foram pegos para treinamento 
+train = data.take(trainSize)                       # take the batches for training
+val = data.skip(trainSize).take(validSize)         # takes the batches for validation skipping the ones that were taken for training
 
-# AI TIME!!!
-
-model = tf.keras.models.Sequential() # inicia o modelo
+# model configuration
+model = tf.keras.models.Sequential()
 
 model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(256,256,3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -62,14 +55,15 @@ model.add(tf.keras.layers.Dense(256, activation='relu'))
 model.add(tf.keras.layers.Dense(6, activation='softmax'))
 
 model.compile(loss=keras.losses.sparse_categorical_crossentropy,optimizer='adam', metrics=['accuracy'],) #compila o modelo
-model.summary() #mostra os resultos do modelo.
+model.summary() #shows the results of the model.
 
-es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=5)
-mc = tf.keras.callbacks.ModelCheckpoint(os.path.join('models','best_model-TEST.h5'), monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
+es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=5) #callback config for early stop the training
+mc = tf.keras.callbacks.ModelCheckpoint(os.path.join('models','best_model-TEST.h5'), monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)#callback config for model checkpoint
 
-hist = model.fit(train, epochs=20, validation_data=val,verbose="auto",callbacks=[mc]) # treinamento 
+hist = model.fit(train, epochs=20, validation_data=val,verbose="auto",callbacks=[mc]) # training
 model.save(os.path.join('models','imageclassifier-dataset-bom-45k.h5'))
-# plota o resultado
+
+# plot a graphic of the training results 
 plt.plot(hist.history['loss'], label='Loss training data')
 plt.plot(hist.history['val_loss'], label='Loss Validation Data')
 plt.plot(hist.history['accuracy'], label='Accuracy training data')
@@ -81,6 +75,5 @@ plt.show()
 
 
 # evaluate
-loss,accuracy = model.evaluate(val) # testa dois batches
-print(loss,accuracy) #mostra os resultados do teste
-
+loss,accuracy = model.evaluate(val) 
+print(loss,accuracy)
